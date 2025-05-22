c4ea2d458e32f24d800a105f7bc1b3f86290737d
Akhil18 <akhilgoud224@gmail.com>
2025-05-22 20:27:22 +0530

    second commit

--- request.py
+++ request.py
@@ -1,10 +1,10 @@
 import requests
 
-url = "http://localhost:8000/api/messages/"
+endpoint = "http://localhost:8000/api/messages/"
 data = {
     "date": "2025-05-22",
     "time": "19:54", 
     "message": "message display here!"
 }
-response = requests.post(url, json=data)
+response = requests.post(endpoint, json=data)
 print(response.json())
