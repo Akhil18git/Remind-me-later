011b07b3f531b939aa74e812fd1824a9470953aa
Akhil18 <akhilgoud224@gmail.com>
2025-05-23 10:40:28 +0530

    final commit

--- request.py
+++ request.py
@@ -1,10 +1,11 @@
 import requests
 
-endpoint = "http://localhost:8000/api/messages/"
+BASE_URL = "http://localhost:8000/"
+ENDPOINT = "api/messages/"
 data = {
     "date": "2025-05-22",
     "time": "19:54", 
     "message": "message display here!"
 }
-response = requests.post(endpoint, json=data)
+response = requests.post(BASE_URL + ENDPOINT, json=data)
 print(response.json())
